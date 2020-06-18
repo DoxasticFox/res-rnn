391783794dec21abc90a7f5b1c58028b5651b581
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2020-06-18 20:04:13 +1000

    Delete orthogonality loss

--- b.py
+++ b.py
@@ -5,19 +5,17 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import nnmodules
-import loss
-import matplotlib.pyplot as plt
 
 # Check Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Define Hyper-parameters
 input_size = 784
-state_size = 256
+state_size = 1750
 output_size = 10
 num_epochs = 10000
-train_batch_size = 500
-test_batch_size = 100
+train_batch_size = 45
+test_batch_size = 45
 
 # MNIST dataset
 train_dataset = torchvision.datasets.MNIST(root='data',
@@ -33,70 +31,53 @@ test_dataset = torchvision.datasets.MNIST(root='data',
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                            batch_size=train_batch_size,
                                            shuffle=True,
-                                           pin_memory=True,
-                                           num_workers=4)
+                                           pin_memory=True)
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=test_batch_size,
                                           shuffle=False,
-                                          pin_memory=True,
-                                          num_workers=4)
+                                          pin_memory=True)
 
-class EncoderDecoderRnn(torch.nn.Module):
-    def __init__(self):
-        super(EncoderDecoderRnn, self).__init__()
-
-        self.encoder = nnmodules.ResRnn(
-            input_width=1,
-            state_width=state_size,
-            output_width=1 + state_size
-        )
-        self.decoder = nnmodules.ResRnn(
-            input_width=0,
-            state_width=1 + state_size,
-            output_width=28
-        )
-
-    def forward(self, input):
-        encoded = self.encoder(input, output_indices=-1)
-        decoded = self.decoder(encoded, max_iterations=28)
-        return decoded
-
-model = EncoderDecoderRnn().to(device)
-
+model = nnmodules.ResRnn(
+    input_width=1,
+    state_width=state_size,
+    output_width=output_size,
+    linearity=0.99999
+).to(device)
 smooth_l1_loss = torch.nn.SmoothL1Loss().to(device)
 
-optimizer = torch.optim.SGD(model.parameters(), lr=10000.0, momentum=0.9)
+# Loss and optimizer
+def loss_fn(outputs, labels):
+    one_hot = torch.nn.functional.one_hot(labels, num_classes=output_size).type(outputs.dtype)
+
+    return smooth_l1_loss(outputs, one_hot)
+
+optimizer = torch.optim.SGD(model.parameters(), lr=10000.00, momentum=0.9)
 
 # Train the model
 total_step = len(train_loader)
 step_num = 0
 
 for epoch in range(num_epochs):
-    for i, (images, _) in enumerate(train_loader):
-        images = images \
-            .reshape(-1, 28 * 28, 1) \
-            .expand(-1, -1, -1) \
-            .permute(1, 0, 2) \
-            .to(device)
+    for i, (images, labels) in enumerate(train_loader):
+        padding = torch.zeros((images.size(0), 3216,))
+        images = images.reshape(images.size(0), 28 * 28)
+        images = torch.cat([images, padding], dim=1)
+        images = images.reshape(images.size(0), -1, 1)
+        images = images.permute(1, 0, 2)
+        images = images.to(device)
+
+        labels = labels.to(device)
 
         # Forward pass
-        outputs = model(images)
-        total_loss = smooth_l1_loss(
-            images.permute(1, 0, 2).reshape(-1, 28, 28).permute(1, 0, 2),
-            outputs
-        )
+        outputs = model(images, output_indices=-1)
+        total_loss = loss_fn(outputs, labels)
 
         # Backprpagation and optimization
         optimizer.zero_grad()
         total_loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.001)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.0001)
         optimizer.step()
-
-        if step_num % 10500 == 0:
-            plt.imshow(outputs.permute(1, 0, 2)[0].cpu().detach().numpy(), cmap='gray_r')
-            plt.show()
-
         step_num += 1
 
         if step_num % 10 == 0:
@@ -112,3 +93,28 @@ for epoch in range(num_epochs):
                     total_loss.item()
                 )
             )
+
+        # Test the model
+        # In the test phase, don't need to compute gradients (for memory efficiency)
+        if step_num % 600 == 0 or i == 1:
+            with torch.no_grad():
+                correct = 0
+                total = 0
+                for images_, labels_ in test_loader:
+                    padding = torch.zeros((images_.size(0), 3216,))
+                    images_ = images_.reshape(images_.size(0), 28 * 28)
+                    images_ = torch.cat([images_, padding], dim=1)
+                    images_ = images_.reshape(images_.size(0), -1, 1)
+                    images_ = images_.permute(1, 0, 2)
+                    images_ = images_.to(device)
+
+                    labels_ = labels_.to(device)
+                    outputs = model(images_, output_indices=-1)
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels_.size(0)
+                    correct += (predicted == labels_).sum().item()
+
+                print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
+
+# Save the model checkpoint
+torch.save(model.state_dict(), 'model.ckpt')
