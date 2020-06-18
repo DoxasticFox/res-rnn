0ac07ebefdaa03b19454293f8dd1a4f762f3a145
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2020-06-19 08:11:48 +1000

    Simplify ResRnn. RAM usage is bad though.

--- nnmodules/__init__.py
+++ nnmodules/__init__.py
@@ -24,16 +24,17 @@ def _broadcast_but_last(x, y):
     else:
         return new, y
 
-class Shift(torch.nn.Module):
+class ShiftRight(torch.nn.Module):
     def __init__(self):
-        super(Shift, self).__init__()
+        super(ShiftRight, self).__init__()
 
-    def forward(self, x, s):
-        x_original_width = x.size(-1)
+    def forward(self, s, x, new_width=None):
+        output_width = new_width if new_width is not None else x.size(-1)
+        assert(output_width <= s.size(-1) + x.size(-1))
 
-        x, s = _broadcast_but_last(x, s)
+        s, x = _broadcast_but_last(s, x)
         x = torch.cat((s, x), dim=-1)
-        x = x[..., :x_original_width]
+        x = x[..., :output_width]
 
         return x
 
@@ -79,7 +80,7 @@ class ShiftedResNet(torch.nn.Module):
         self.fc1 = torch.nn.Linear(input_width, hidden_width)
 
         self.shf = torch.nn.ModuleList(
-            Shift() for _ in range(depth))
+            ShiftRight() for _ in range(depth))
         self.res = torch.nn.ModuleList(
             Res(hidden_width, linearity) for _ in range(depth))
 
@@ -104,7 +105,7 @@ class ResRnn(torch.nn.Module):
         self,
         input_width,
         state_width,
-        output_width=None,
+        output_width,
         linearity=0.99999
     ):
         super(ResRnn, self).__init__()
@@ -115,122 +116,66 @@ class ResRnn(torch.nn.Module):
         self.stream_width = input_width + state_width
         self.linearity = linearity
 
-        self.output_width = output_width or self.stream_width
-
         assert(self.output_width <= self.stream_width)
         assert(self.input_width >= 0)
         assert(self.state_width >= 0)
-        assert(self.stream_width > 0)
 
-        self.initial_stream = torch.nn.Parameter(
-            torch.zeros(size=(self.stream_width,)),
+        self.initial_state = torch.nn.Parameter(
+            torch.zeros(size=(self.state_width,)),
         )
-        self.do_terminate = None
 
-        self.ins = Shift()
+        self.ins = ShiftRight()
         self.res = Res(self.stream_width, self.linearity)
 
-    def forward(
-        self,
-        input,
-        output_indices=None,
-        term_fun=None,
-        max_iterations=None
-    ):
+    def forward(self, input, state=None):
         # input:
         #     (seq_width, batch_width, input_width) or
-        #     (batch_width, state_width)
-        # output_stream:
+        # state:
+        #     (batch_width, state_width) or
+        #     (state_width)
+        # output_state:
         #     (seq_width, batch_width, output_width)
         # returns:
-        #      if output_indices == -1:
-        #          (batch_width, output_width)
-        #      elif output_indices == None:
-        #          (seq_length, batch_width, output_width)
-        #      elif output_indices is list-like:
-        #          (batch_width, output_width)
+        #     (seq_width, batch_width, output_width) and
+        #     (seq_width, batch_width, state_width)
 
-        # Check inputs and unpack input size
-        seq_width, batch_width, input_width, state_width = 0, 0, 0, 0
-        try:
-            seq_width, batch_width, input_width = input.size()
-        except ValueError:
-            pass
-        try:
-            batch_width, state_width = input.size()
-        except ValueError:
-            pass
+        input_seq_width, input_batch_width, input_width = input.size()
 
-        assert(input_width == 0 or input_width == self.input_width)
-        assert(
-            output_indices is None or
-            output_indices == -1 or
-            output_indices.size() == torch.Size((batch_width,))
-        )
-        assert(
-            type(output_indices) != torch.Tensor or
-            output_indices.min() >= 0)
-        assert(
-            type(output_indices) != torch.Tensor or
-            output_indices.max() < seq_width)
-        assert(
-            input_width > 0 or
-            term_fun is not None or
-            max_iterations is not None
-        )
+        state_batch_width, state_width = None, None
+        if state:
+            try:
+                state_batch_width, state_width = state.size()
+            except ValueError:
+                pass
 
-        # Apply RNN
-        if input_width == 0 and self.do_terminate is None and term_fun:
-            self.do_terminate = torch.tensor(
-                [False] * batch_width,
-                device=input.device
-            )
-        if input_width == 0 and term_fun:
-            self.do_terminate = self.do_terminate.zero_()
+            try:
+                state_width = state.size()
+            except ValueError:
+                pass
 
-        outputs = []
-        for index in itertools.count():
-            if input_width == 0 and index >= max_iterations:
-                break
-            if input_width == 0 and term_fun and outputs:
-                self.do_terminate |= term_fun(outputs[-1])
-                if self.do_terminate.all():
-                    break
-            if input_width > 0 and index == seq_width:
-                break
+        assert(                 input_width == self.input_width)
+        assert(state == None or state_width == self.state_width)
+        assert(state == None or state_batch_width == input_batch_width)
+        assert(state == None or input_seq_width == state_batch_width)
 
-            # Prepare first input to RNN
-            if input_width == 0 and index == 0:
-                output_stream = input
-            if input_width > 0 and index == 0:
-                output_stream = (1 - self.linearity) * self.initial_stream
+        if state is None:
+            stream = (1 - self.linearity) * self.initial_state
+        else:
+            stream = state
 
-            # Insert the input into the stream
-            if input_width > 0:
-                output_stream = self.ins(output_stream, input[index])
+        streams = []
+        for element in input:
+            stream = self.ins(element, stream, self.stream_width)
+            stream = self.res(stream)
 
-            # Perform a single ResRNN application
-            output_stream = self.res(output_stream)
+            streams.append(stream)
 
-            # Take outputs from the high indices because it might help with
-            # learning long distance dependencies.
-            output = output_stream[..., -self.output_width:]
-            outputs.append(output)
+        streams = torch.stack(streams)
 
-        # Collect the outputs we want.
-        #   * Return the outputs from the last RNN application in every batch.
-        #     If the sequences do not have the same length, the shorter
-        #     sequences would have had the RNN applied to them a few extra times
-        #     needlessly.
-        if output_indices == -1:
-            return outputs[-1]
-        outputs = torch.stack(outputs)
-        #   * Return the outputs from every RNN application in every batch.
-        if output_indices is None:
-            return outputs
-        #   * Return the outputs from the last RNN application to each sequence
-        #     in the batch.
-        if type(output_indices) == torch.Tensor:
-            return outputs[output_indices, torch.range(batch_width)]
+        # We take the last output elements instead of the first because we
+        # hypothesise that this will make learning long distance dependencies
+        # easier.
+        outputs = streams[..., -self.output_width:]
+        states  = streams[..., :-self.output_width]
 
-        raise RuntimeError('Failed to collect output')
+        return outputs, states
---
+++ translate.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import nnmodules
+
+# Check Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Define Hyper-parameters
+input_size = 784
+state_size = 512
+output_size = 10
+num_epochs = 10000
+train_batch_size = 512
+test_batch_size = 100
+
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='data',
+                                           train=True,
+                                           transform=transforms.ToTensor(),
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='data',
+                                          train=False,
+                                          transform=transforms.ToTensor())
+
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=train_batch_size,
+                                           shuffle=True,
+                                           pin_memory=True,
+                                           num_workers=4)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=test_batch_size,
+                                          shuffle=False,
+                                          pin_memory=True,
+                                          num_workers=4)
+
+model = nnmodules.ResRnn(
+    input_width=1,
+    state_width=state_size,
+    output_width=output_size,
+).to(device)
+smooth_l1_loss = torch.nn.SmoothL1Loss().to(device)
+
+# Loss and optimizer
+def loss_fn(outputs, labels):
+    one_hot = torch.nn.functional.one_hot(labels, num_classes=output_size).type(outputs.dtype)
+
+    return smooth_l1_loss(outputs, one_hot)
+
+optimizer = torch.optim.SGD(model.parameters(), lr=100000.0, momentum=0.9)
+
+# Train the model
+total_step = len(train_loader)
+step_num = 0
+
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.reshape(-1, 28 * 28, 1).permute(1, 0, 2).to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs, _ = model(images)
+        outputs = outputs[-1]
+        total_loss = loss_fn(outputs, labels)
+
+        # Backprpagation and optimization
+        optimizer.zero_grad()
+        total_loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.0001)
+        optimizer.step()
+        step_num += 1
+
+        if step_num % 10 == 0:
+            print(
+                'Epoch [{}/{}], '
+                'Step [{}/{}], '
+                'Total loss: {:.4f}'
+                .format(
+                    epoch+1,
+                    num_epochs,
+                    i+1,
+                    total_step,
+                    total_loss.item()
+                )
+            )
+
+        # Test the model
+        # In the test phase, don't need to compute gradients (for memory efficiency)
+        if step_num % 600 == 0:
+            with torch.no_grad():
+                correct = 0
+                total = 0
+                for images_, labels_ in test_loader:
+                    images_ = images_.reshape(-1, 28 * 28, 1).permute(1, 0, 2).to(device)
+                    labels_ = labels_.to(device)
+                    outputs, _ = model(images_)
+                    outputs = outputs[-1]
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels_.size(0)
+                    correct += (predicted == labels_).sum().item()
+
+                print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
