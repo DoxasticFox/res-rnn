952ccdc20c11c6c73304abf69524ef32a7817130
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2020-07-01 03:39:59 +1000

    Add pmnist-test-4000.py

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 A recurrent neural network with residual connections.
 
-## Weird Stuff
+## Random
 
-* The learning rate is really, really high (e.g. 10000.0) because of the way the residual connections have been parameterised.
+* The 4000-dimensional models gets SOTA on pMNIST, even beating fake RNNs, like [TrellisNets](https://arxiv.org/pdf/1810.06682.pdf) and [IGLOO](https://arxiv.org/pdf/1807.03402.pdf).
+* The learning rate is really, really high (e.g. 1e+5) because of the way the residual connections have been parameterised.
---
+++ pmnist-test-4000.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import nnmodules
+
+# Check Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# MNIST dataset
+test_dataset = torchvision.datasets.MNIST(root='data',
+                                          train=False,
+                                          transform=transforms.ToTensor())
+
+# Data loader
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=10000,
+                                          shuffle=False,
+                                          pin_memory=True)
+
+model = nnmodules.ResRnn(
+    input_width=1,
+    state_width=4000,
+    output_width=10,
+    linearity=0.99999,
+).to(device)
+
+model.load('models/pmnist-1-4000-10-98.45.pt')
+
+# Loss and optimizer
+
+torch.manual_seed(0)
+random_indices = torch.randperm(28 * 28)
+
+# Train the model
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.reshape(images.size(0), 28 * 28)
+        images = images[:, random_indices]
+        images = images.reshape(images.size(0), 28 * 28, 1)
+        images = images.permute(1, 0, 2)
+        images = images.to(device)
+
+        labels = labels.to(device)
+        outputs, _ = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
